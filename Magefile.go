045ba541f1f5b0c2dabca688825d0b9a4c2c1996
Chris Suszyński <ksuszyns@redhat.com>
2020-11-13 01:18:34 +0100

    Update megetasks

--- Magefile.go
+++ Magefile.go
@@ -4,62 +4,20 @@ package main
 
 import (
 	"log"
-	"os"
-	"path"
 
 	"github.com/cardil/homedir/internal"
 	"github.com/joho/godotenv"
-	"github.com/magefile/mage/mg"
-	"github.com/magefile/mage/sh"
 
 	// mage:import
 	"github.com/wavesoftware/go-magetasks"
 	"github.com/wavesoftware/go-magetasks/config"
-	"github.com/wavesoftware/go-magetasks/pkg/tasks"
+
+	// mage:import
+	_ "github.com/wavesoftware/go-magetasks/container"
 )
 
 // Default target is set to binary
 var Default = magetasks.Binary
-
-// Publish will publish built images to a remote registry
-func Publish() {
-	mg.Deps(Images)
-	if len(config.Binaries) > 0 {
-		t := tasks.StartMultiline("📤", "Publishing OCI images")
-		errs := make([]error, 0)
-		for _, binary := range config.Binaries {
-			args := []string{
-				"push", imagename(binary),
-			}
-			args = append(args)
-			err := sh.RunV("podman", args...)
-			errs = append(errs, err)
-		}
-		t.End(errs...)
-	}
-}
-
-// Images builds a OCI images of binaries
-func Images() {
-	mg.Deps(magetasks.Binary)
-
-	if len(config.Binaries) > 0 {
-		t := tasks.StartMultiline("📦", "Packaging OCI images")
-		errs := make([]error, 0)
-		for _, binary := range config.Binaries {
-			args := []string{
-				"build",
-				"-f", containerfile(binary),
-				"-t", imagename(binary),
-				".",
-			}
-			args = append(args)
-			err := sh.RunV("podman", args...)
-			errs = append(errs, err)
-		}
-		t.End(errs...)
-	}
-}
 
 func init() {
 	err := godotenv.Load()
@@ -70,12 +28,4 @@ func init() {
 		Name: internal.BinaryName,
 	})
 	config.VersionVariablePath = "github.com/cardil/homedir/internal.Version"
-}
-
-func containerfile(bin config.Binary) string {
-	return path.Join("cmd", bin.Name, "Containerfile")
-}
-
-func imagename(bin config.Binary) string {
-	return os.Getenv("CONTAINER_BASENAME") + bin.Name
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/magefile/mage v1.10.0
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/wavesoftware/go-magetasks v0.2.0
+	github.com/wavesoftware/go-magetasks v0.3.0
 )
